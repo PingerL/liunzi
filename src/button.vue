07726e23d680b011712e5076926da64cdfbcd915
pinger <402434104@qq.com>
2019-12-08 22:08:49 +0800

    加入 parcel， 将 button 组件改为单文件组件 ； 将 .cache and dist 加入 .gitignore中

---
+++ src/button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style lang="scss">
+ .g-button {
+      font-size: var(--font-size);
+      height: var(--button-height);
+      padding: 0 1em; /*按钮不用写width,用padding来做就好了，两边各空出一个字的位置*/
+      border-radius: 1px solid var(--border-radius);
+      border: 1px solid var(--border-color);
+      background: var(--button-bg);
+      &:hover{
+        border-color: var(--border-color-hover);
+      }
+      &:active{
+        background: var(--button-active-bg);
+      }
+      &:focus{
+        outline: none;
+      }
+    }
+   
+</style>
