74965ed88556e43a983bca459064c540dcf88e1e
pinger <402434104@qq.com>
2019-12-09 12:12:26 +0800

    添加 icon 和 iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,28 +1,55 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg class="icon" v-if="icon">
+      <use :xlink:href="`#i-${icon}`" />
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {
-    
-  }
+export default {
+  props: ["icon", "iconPosition"] //props:接收父组件传递过来的参数
+};
 </script>
 <style lang="scss">
- .g-button {
-      font-size: var(--font-size);
-      height: var(--button-height);
-      padding: 0 1em; /*按钮不用写width,用padding来做就好了，两边各空出一个字的位置*/
-      border-radius: 1px solid var(--border-radius);
-      border: 1px solid var(--border-color);
-      background: var(--button-bg);
-      &:hover{
-        border-color: var(--border-color-hover);
-      }
-      &:active{
-        background: var(--button-active-bg);
-      }
-      &:focus{
-        outline: none;
-      }
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em; /*按钮不用写width,用padding来做就好了，两边各空出一个字的位置*/
+  border-radius: 1px solid var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em;
     }
-   
+    > .content {
+      order: 1;
+    }
+  }
+}
 </style>
