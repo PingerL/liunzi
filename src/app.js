df64566233bd3b400e6f504045c518b3d837652e
pinger <402434104@qq.com>
2019-12-17 13:25:25 +0800

    使用 chai.expect 添加测试用例，第五个不会

--- src/app.js
+++ src/app.js
@@ -13,3 +13,93 @@ new Vue({
       loading3: false
     }
 })
+
+// 单元测试,使用 chai 来做单元测试
+import chai from 'chai'
+const expect = chai.expect
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings'
+    }
+  })
+  console.log(vm)
+  vm.$mount('#test') //动态生成 button,并将其挂在页面上
+
+  let useElement = vm.$el.querySelector('use')
+  console.log(useElement)
+
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-settings')
+  vm.$el.remove()  //删除元素
+  vm.$destroy()   //把vm对像删掉，不然会占用内存
+
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData:{
+      icon:'settings',
+      loading: true
+    }
+  })
+
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()  //删除元素
+  vm.$destroy()   //把vm对像删掉，不然会占用内存
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon:'settings'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  vm.$el.remove()  //删除元素
+  vm.$destroy()   //把vm对像删掉，不然会占用内存
+}
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon:'settings',
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  vm.$el.remove()  //删除元素
+  vm.$destroy()   //把button对像删掉，不然会占用内存
+}
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon:'settings',
+    }
+  })
+  vm.$mount()
+  // 判断点击事件是否执行
+  vm.$on('click',function(){
+    expect(1).to.eq(1)  // 一般人是想到这样断言的
+  })
+  let button = vm.$el
+  button.click()
+}
