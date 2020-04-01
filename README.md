2c7c016dadb4b82c5887b043232f5ef8d711bca3
pinger <402434104@qq.com>
2020-04-01 13:28:44 +0800

    添加 input.vue 及其样式

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 # 轮子 - 一个Vue UI 组件
 
+## 介绍
 
-作者：Pinger
-
-## 安装
+## 开始使用
+1. 安装
 使用本框架前，请在 CSS 中开启 border-box
 ```
 *{box-sizing:border-box}
 ```
+
+## 文档
+
+## 提问
+
+## 变更记录
+
+## 联系方式
+
+## 贡献代码
+
+作者：Pinger
+
+
 ## 运行脚本测试
 - 使用 `npm run test` 一次性运行，使用` npm run dev-test` 进行` watch` 运行
 - Windows 用户运行 `npm run dev-test` 时会出现 BUG，解决办法是：
---
+++ src/input.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper" :class="{'error': error}">
+    <input type="text" :placeholder="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <icon  name="error"></icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+import Icon from './icon'
+export default {
+  components:{Icon},
+  props:{
+    value:{
+      type:String
+    },
+    disabled:{
+      type: Boolean,
+      default:false
+    },
+    readonly:{
+      type: Boolean,
+      default:false
+    },
+    error:{
+      type:String
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$font-size: 12px;
+$border-radius:4px;
+.wrapper {
+  margin-bottom: 10px;
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  > :not(:last-child){
+    margin-right: .5em;
+  }
+  > input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    outline: none;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px rgba($color: #f597e5, $alpha: .5);
+    }
+    &[disabled],&[read-only] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  &.error{
+    > input {
+      border-color: #FF6750;
+    }
+    .error-message {
+      color: #FF6750;
+    }
+  }
+}
+</style>
